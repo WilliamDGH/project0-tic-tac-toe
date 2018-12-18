539c1c63c2ce0e974a30e6099f99b6f6c47541d2
Guanhua <william.ting.1116@gmail.com>
2018-12-18 17:12:38 +1100

    removed firebase files

--- js/firebase.js
+++
@@ -1,5 +0,0 @@
-function signIn() {
-  // Sign into Firebase using popup auth & Google as the identity provider.
-  var provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithPopup(provider);
-}
